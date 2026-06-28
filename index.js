e29c1815a942b8dca99bfb22b26175c339569469
Brad Robles <bradmrh@gmail.com>
2026-06-28 13:09:43 -0600

    Verificacion de la conexion con supabase

--- index.js
+++ index.js
@@ -11,3 +11,49 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 console.log("Conexion a la base de datos exitosa.");
+
+
+// Funcion para traer los partidos de la base de datos 
+async function obtenerPartidos() {
+    const { data, error } = await supabase
+    .from('partidos')
+    .select('*');
+
+    if (error) {
+        console.error("Error al obtener los partidos:", error);
+        return;
+    }
+
+    console.log("Conexion exitosa, tus partidos son:");
+    console.log(data);
+}
+
+// Ejecutamos la funcion 
+obtenerPartidos();
+
+// Funcion para insertar un partido en la base de datos desde el codigo 
+async function insertarPartido() {
+    const { data, error } = await supabase 
+    .from('partidos')
+    .insert([
+        {
+            id_local: 1,
+            id_visitante: 2,
+            goles_local: 4,
+            goles_visitante: 1,
+            fecha_partido: '2026-06-28'
+        }
+    ])
+    .select(); // El select es para que nos devuelva el partido creado 
+
+    if (error) {
+        console.error("Error al insertar el partido:", error);
+        return;
+    }
+
+    console.log("Partido insertado exitosamente desde Node.js:");
+    console.log(data);
+}
+
+// Apagamos por un momento la de obtner y ejecutamos la de insertar 
+insertarPartido();

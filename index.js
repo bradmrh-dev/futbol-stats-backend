9865b2d3bea7711f16ae9fca95308998e07d33b5
Brad Robles <bradmrh@gmail.com>
2026-06-29 23:50:59 -0600

    feat: implementar algoritmo para calcular tabla de posiciones
    - Se agrego logica en index.js para recorrer partidos
    
    - Se aplico .trim() a los nombres
    
    - Se ordeno el arreglo y se uso console.table

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // 1. Se cargan las variables de entorno de mi archivo .env
+// DOTENV es un paquete que permite cargar variables de entorno desde un archivo .env a process.env
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
@@ -13,25 +14,84 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 console.log("Conexion a la base de datos exitosa.");
 
 
-// Funcion para traer los partidos de la base de datos 
+// Funcion 1. para traer los partidos de la base de datos 
 async function obtenerPartidos() {
-    const { data, error } = await supabase
+    const { data: partidos, error } = await supabase
     .from('partidos')
-    .select('*');
+    .select(`
+        id,
+        fecha_partido,
+        goles_local, 
+        goles_visitante,
+        id_local( nombre_equipo ),
+        id_visitante( nombre_equipo )
+        `);
 
     if (error) {
         console.error("Error al obtener los partidos:", error);
         return;
     }
 
-    console.log("Conexion exitosa, tus partidos son:");
-    console.log(data);
+    //console.log("Conexion exitosa, tus partidos son:");
+    //console.log(data);
+
+// Funcion 2. Aqui se guardan las estadisticas de los equipos en la base de datos, se ejecuta cada vez que se inserta un partido
+const tablaPosiciones = {};
+
+// Se recorre cada partido para calcular los puntos y goles de cada equipo
+partidos.forEach(partido => {
+    // Se limpian los nombres de los equipos por si tienen salto de linea 
+    const local = partido.id_local.nombre_equipo.trim();
+    const visitante = partido.id_visitante.nombre_equipo.trim();
+
+    const gl = partido.goles_local;
+    const gv = partido.goles_visitante;
+
+    // SI el equipo no existe en la tabla de posiciones, se inicializa con 0 puntos y 0 goles
+    if (!tablaPosiciones[local]) {
+        tablaPosiciones[local] = {equipo: local, pts: 0, pj:0, gf:0, gc:0, dg:0};
+}
+if (!tablaPosiciones[visitante]) {
+    tablaPosiciones[visitante] = {equipo: visitante, pts: 0, pj:0, gf:0, gc:0, dg:0};
 }
 
-// Ejecutamos la funcion 
+// Vamos a sumar partidos jugados, goles a favor y goles en contra 
+tablaPosiciones[local].pj += 1;
+tablaPosiciones[local].gf += gl;
+tablaPosiciones[local].gc += gv;
+
+tablaPosiciones[visitante].pj += 1;
+tablaPosiciones[visitante].gf += gv;
+tablaPosiciones[visitante].gc += gl;
+
+// REGLA: Asignacion de puntos segun el resultado del partido
+if (gl > gv) {
+    tablaPosiciones[local].pts += 3; // Gana el local
+} else if (gl < gv) {
+    tablaPosiciones[visitante].pts += 3; // Gana el visitante
+} else {
+    tablaPosiciones[local].pts += 1; // Empate
+    tablaPosiciones[visitante].pts += 1; // Empate
+}
+
+// Se recalcula la diferencia de goles para cada equipo
+tablaPosiciones[local].dg = tablaPosiciones[local].gf - tablaPosiciones[local].gc;
+tablaPosiciones[visitante].dg = tablaPosiciones[visitante].gf - tablaPosiciones[visitante].gc;
+});
+
+// Convertimos el objeto en un arreglo y lo ordenamos por puntos, de mayor a menor
+const tablaOrdenada = Object.values(tablaPosiciones).sort((a, b) => b.pts - a.pts || b.dg - a.dg);
+
+console.log("\n Tabla de posiciones ");
+console.table(tablaOrdenada); 
+// .table es una funcion de consola que permite mostrar un arreglo de objetos 
+// en forma de tabla
+}
+
+// Ejecutamos la funcion CONTROL
 obtenerPartidos();
 
-// Funcion para insertar un partido en la base de datos desde el codigo 
+// Funcion 3. para insertar un partido en la base de datos desde el codigo 
 async function insertarPartido() {
     const { data, error } = await supabase 
     .from('partidos')
@@ -44,7 +104,7 @@ async function insertarPartido() {
             fecha_partido: '2026-06-28'
         }
     ])
-    .select(); // El select es para que nos devuelva el partido creado 
+    .select();
 
     if (error) {
         console.error("Error al insertar el partido:", error);
@@ -56,4 +116,7 @@ async function insertarPartido() {
 }
 
 // Apagamos por un momento la de obtner y ejecutamos la de insertar 
-insertarPartido();
+// insertarPartido(); // CONTROL: Descomentar esta linea para insertar un partido en la base de datos
+
+
+
